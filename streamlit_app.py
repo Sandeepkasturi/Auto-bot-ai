da8276e82ec24e7bbd248d69251f3e604f74fbe3
Sandeep Kasturi <112337452+Sandeepkasturi@users.noreply.github.com>
2024-04-29 10:35:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,16 @@ from google.generativeai import configure, GenerativeModel
 from IPython.display import Markdown
 import textwrap
 from urllib.parse import quote, urlparse
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
 
 # Set up the Generative AI configuration with a placeholder API key
-configure(api_key=st.secrets["api_key"])
+configure(api_key=st.secrets["api_key"]))
 
 # Create a Generative Model instance (assuming 'gemini-pro' is a valid model)
 model = GenerativeModel('gemini-pro')
 
-
-# Function to convert plain text to Markdown format
 def to_markdown(text):
     text = text.replace('•', '  *')
     return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
@@ -120,7 +121,6 @@ def get_binary_file_downloader_html(bin_file, file_label='Download Code'):
     href = f'<a href="data:file/html;base64,{b64}" download="{bin_file}" target="_blank">{file_label}</a>'
     return href
 
-
 # Function to display footer
 def display_footer():
     # Additional buttons
@@ -128,15 +128,6 @@ def display_footer():
     st.markdown("&copy; 2024 SKAV TECH . All rights reserved.")
     st.subheader('@ Follow on')
     st.markdown("https://bit.ly/socialinstag")
-    st.markdown("<html>google-site-verification: google543c77f631827437.html</html>", unsafe_allow_html=True)
-    st.markdown(
-        """<meta name="google-site-verification" content="P_V7QpJhhNnZfjp1lndSh-zM1mwA3nM4o_VnKBmiv_Y"/></meta>""",
-        unsafe_allow_html=True)
-
-
-# Global variable to track whether the VS Code page is Active
-colab_code_active = False
-
 
 # Main Streamlit application
 def main():
@@ -150,29 +141,21 @@ def main():
     st.sidebar.info(
         "Support the Project.")
     st.sidebar.info("""
-         **UPI ID's**
-
-         1. Phonepe: 9919932723@ybl
-         2. Gpay: sandeepkasturi67@oksbi
-         3. Paytm: 9919932723@paytm
+        Payment Details:
+         1. **UPI NUMBER: 9919932723**
+         2. **Name: Kasturi Sandeep**
          """)
 
     st.sidebar.markdown("Click here to UPI QR CODE: "
-                        f'<a href="https://ibb.co/nBtGVnk" download="downloaded_image.png" alt = "UPI QR"><img src="https://ibb.co/nBtGVnk" width="400"></a>',
+                        f'<a href="https://ibb.co/nBtGVnk" download="downloaded_image.png" alt = "UPI"><img src="https://i.ibb.co/0Kv7WFJ/Google-Pay-QR.png" width="50"></a>',
                         unsafe_allow_html=True
                         )
 
     # Display image with a link for download
-    st.sidebar.write("Thank you for your support!")
+    st.sidebar.write("Thank you for your support ❤️")
 
     if page == "🏠 Home":
-
-        st.write("**Below is a quick tutorial video on how to use the application**:")
-        # Replace 'video_url' with the URL of your tutorial video
-        video_url = "https://youtu.be/i0Q-NBrYpPI"
-        # Embed the video with autoplay
-        st.video(video_url, start_time=0)
-        st.title("Welcome to AutoBot AI")
+        st.title("Welcome to AutoBot 🤖")
         st.markdown(""" 
         **AutoBot AI**:
          **Functionalities:**
@@ -189,15 +172,22 @@ def main():
 
         Developed by SKAV TECH, a company focused on creating practical AI projects, AutoBot is intended for educational purposes only. We do not endorse any illegal or unethical activities.""")
 
+        st.write("**Watch the Quick tutorial on how to use the application**:")
+        # Replace 'video_url' with the URL of your tutorial video
+
+        if st.button("Tutorial"):
+            video_url = "https://youtu.be/i0Q-NBrYpPI"
+            # Embed the video with autoplay
+            st.video(video_url, start_time=0)
+
         # Display footer
         display_footer()
-
 
     elif page == "AutoBot 🤖":
         st.image("auto_bot_2.png")
         st.header("AutoBot 🤖")
         st.markdown(
-            "Ask the AI to generate codes for your use-case. You can now get downloadable files whenever the keyword *CODE* is in your prompt.")
+            "AutoBot is effective for code generation, if your prompt contains code generation then you can get Downloadable files")
         question = st.text_input("Ask the model a question:")
         if st.button("Ask AI"):
             try:
@@ -207,7 +197,8 @@ def main():
                 st.write(response.text)
                 st.markdown('---')
                 st.markdown("""Security Note:
-    We used .TXT file format for Code downloads which is not easily possible for virus and malware attacks""")
+                We used **.txt** file format for Code downloads which is not easily possible for virus and malware attacks""")
+
             except ValueError:
                 st.write(
                     "**Sorry 😖, I can't assist you with that. I feel it's sensitive or Confidential or out of my Knowledge. Please ask something else.**")
@@ -224,130 +215,71 @@ def main():
 
         # Display footer
         display_footer()
-    # Add the implementation of the VS Code page to your main Streamlit application
+
     elif page == "Github CodeSpaces 🖥️":
-        global colab_code_active
-        colab_code_active = True
         st.header("Github CodeSpaces 🖥️")
-        if colab_code_active:
-            st.markdown("Application will start in 10 sec. 😀")
-            colab_code()
+        st.markdown("Application will start in 10 sec. 😀")
+        colab_code()
 
     elif page == "Web Scrapper 🌐":
-
         st.header("Web Scrapper")
-
         st.markdown(
-
             "AutoBot powered **Web Scrapper**. This tool will get the code of any website. Simply enter the URL below. Download Extracted Code. Upload the Downloaded HTML code in the HTML editor section, then run the application")
-
         url = st.text_input("Enter URL:")
-
         if st.button("Extract HTML Code"):
-
             try:
-
                 response = requests.get(url)
-
                 if response.status_code == 200:
-
                     extracted_html = response.text
-
                     # Pass the URL to the download_html_code function
-
                     download_html_code(extracted_html, url)
-
                 else:
-
                     st.error(f"Failed to retrieve HTML content 😕. Status code: {response.status_code}")
-
             except Exception as e:
-
                 st.error(f"An error occurred check the URL 🌐. Or May be system cannot retrieve content 😕 : {str(e)}")
-
         # Display footer
-
         display_footer()
-        # Update the HTML Editor section
 
     elif page == "CODEX ⚡":
         st.header("CODEX ⚡️")
-        st.info("""
+        uploaded_files = st.file_uploader("Upload code files:", accept_multiple_files=True)
+        st.markdown("""
                 Welcome to **CODEX** 
-                Hi I'm CODEX your interactive CODING GUIDE, No one can explain Code like me Trust me.
+                Interactive CODING GUIDE, No one can explain Code like me trust me,
                 Trust is the Most expensive thing Cheap people can't afford it. 
                 1. You can ask specific code or content using the phase @codex "prompt"
                 2. You can upload your code here to ask the CODEX to generate Explanation (for eg. @codex Can you explain me this code in the file "filename"
                 3. I can Generate 60 queries per minute Pretty wild right, Haha more to see and Explore.
                 """)
-
-        st.info("use @codex phase to start the prompt")
-        st.info("""Example prompt: @codex explain me the code in the file "your_file_name
+        st.info("""Example prompt: @codex explain me the code in the file "your_file_name"
 
         Some popular Prompts:. 
                 1. @codex explain me the code in the file app.py
                 2. @codex how does this code works in the file app.py
-
-
+                3. @codex can you explain me this code how does it works: "paste your code"
         """)
-        prompt = st.text_area('Type your query here:', height=100)
+        st.warning("use @codex phase to start the prompt")
+        prompt = st.text_area('Type your query here:', height=300)
         st.markdown('---')
-        uploaded_files = st.file_uploader("Upload code files:", accept_multiple_files=True)
-        if st.button('Submit Query'):
-            if uploaded_files:
-                # Check for the presence of the uploaded file and the query
+        if st.button('Submit'):
+            if prompt or uploaded_files:
                 if prompt:
-                    query_keywords = ["file", "code file", "application"]
-
-                    # Convert prompt to lowercase for case-insensitive comparison
-                    prompt_lower = prompt.lower()
-
-                    # Check if any query keyword is present in the prompt
-                    if any(keyword in prompt_lower for keyword in query_keywords):
-                        # Identify the filename mentioned in the query
-                        file_name = None
-                        for keyword in query_keywords:
-                            if keyword in prompt_lower:
-                                # Find the keyword's position in the prompt
-                                keyword_idx = prompt_lower.index(keyword)
-                                # Extract the filename mentioned after the keyword
-                                file_name = prompt[keyword_idx + len(keyword):].strip()
-                                break
-
-                        if file_name:
-                            # Look for the specified file in the uploaded files
-                            requested_file = None
-                            for file in uploaded_files:
-                                if file_name.lower() in file.name.lower():
-                                    requested_file = file
-                                    break
-
-                            if requested_file:
-                                # Get the contents of the requested file
-                                file_contents = requested_file.read().decode("utf-8")
-
-                                # Call the generative model with the file contents as input
-                                response = model.generate_content(file_contents)
-
-                                # Display file content and CODEX response
-                                st.write(f"Code for {requested_file.name}:")
-                                st.code(file_contents)  # Display file content as code
-
-                                st.write("CODEX Response:")
-                                st.write(response.text)  # Display model response
-                            else:
-                                st.error(f"File named '{file_name}' not found among the uploaded files.")
-                        else:
-                            st.error("Please provide a valid query mentioning the uploaded file.")
-                    else:
-                        st.error("Please include a keyword ('file', 'code file', 'application') in your query.")
+                    response = model.generate_content(prompt)
+                    st.write("CODEX Response:")
+                    st.write(response.text)
+                if uploaded_files:
+                    for file in uploaded_files:
+                        st.write(f"Code for {file.name}:")
+                        st.code(file.getvalue())  # Display file content as code
+                        # Call the generative model with the file contents as input
+                        response = model.generate_content(file.getvalue())
+                        st.write("CODEX Response:")
+                        st.write(response.text)  # Display model response
             else:
-                st.error("Please upload a file.")
-            # Display the compiled HTML
-            # display_html(html_code)
+                st.error("Please provide a query or upload a file.")
+
         # Display footer
         display_footer()
-
 
 if __name__ == "__main__":
     main()
